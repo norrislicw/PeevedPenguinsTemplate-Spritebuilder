7dbf94cc1ba51b7b46d06fae63a14364d474f0ba
Norris <Norris@Norris-MacBook-Pro.local>
2014-11-10 21:16:09 +0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -38,7 +38,7 @@
     
     // manually create & apply a force to launch the penguin
     CGPoint launchDirection = ccp(1, 0);
-    CGPoint force = ccpMult(launchDirection, 8000);
+    CGPoint force = ccpMult(launchDirection, 6000);
     [penguin.physicsBody applyForce:force];
     
     // ensure followed object is in visible are when starting
