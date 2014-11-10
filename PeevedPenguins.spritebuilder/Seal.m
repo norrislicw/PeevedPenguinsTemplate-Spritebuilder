c6971393ec73a3d4b805940c0d2a9a9838948d74
Norris <Norris@Norris-MacBook-Pro.local>
2014-11-10 20:07:24 +0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Seal.m
+++ PeevedPenguins.spritebuilder/Seal.m
@@ -1,20 +1,20 @@
 //
-//  Penguin.m
+//  Seal.m
 //  PeevedPenguins
 //
 //  Created by Norris on 10/11/14.
 //  Copyright (c) 2014 Apportable. All rights reserved.
 //
 
-#import "Penguin.h"
+#import "Seal.h"
 
-@implementation Penguin
+@implementation Seal
 
 - (id)init {
     self = [super init];
     
     if (self) {
-        CCLOG(@"Penguin created");
+        CCLOG(@"Seal created");
     }
     
     return self;
