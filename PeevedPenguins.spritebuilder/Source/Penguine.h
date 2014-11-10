4c56d16f2be4248f0a088a81f2c50226014dca89
Norris <Norris@Norris-MacBook-Pro.local>
2014-11-10 19:33:05 +0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguine.h
@@ -0,0 +1,13 @@
+//
+//  Penguine.h
+//  PeevedPenguins
+//
+//  Created by Norris on 10/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguine : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Penguine.m
@@ -0,0 +1,23 @@
+//
+//  Penguine.m
+//  PeevedPenguins
+//
+//  Created by Norris on 10/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguine.h"
+
+@implementation Penguine
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+
+@end
